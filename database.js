d07c7d9f717af82f2809c0c0576a5c8deeb11998
urbanykv <matheusurban19@gmail.com>
2023-10-07 12:46:16 -0300

    API

---
+++ database.js
@@ -0,0 +1,6 @@
+const produto = {
+    nome: 'Samsung S8',
+    preco: 1.099
+}
+
+export default produto
---
+++ index.js
@@ -0,0 +1,14 @@
+const produto = {
+    nome: 'Samsung S8',
+    preco: 1.099
+}
+
+const jsonProduto = JSON.stringify(produto);
+
+const express = require('express');
+
+const app = express();
+
+app.get('/produto/1', (req, res) => {
+    res.send(jsonProduto);
+})
