cf4937d90fa69098de37a6c25660a1fd293f8323
urbanykv <matheusurban19@gmail.com>
2023-10-09 15:55:35 -0300

    Express

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 const produto = {
     nome: 'Samsung S7',
     preco: 1500,
-    marca: 'Samsumg'
+    marca: 'Samsung'
 };
 
 app.get('/', (req, res) => {
     res.json(produto);
 })
 
-app.listen(80, () => {
+app.listen(5500, () => {
     console.log('Server iniciado!');
 })
